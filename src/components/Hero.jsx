ecdc859064ee7bc1e0796398b0cd88f273549966
Anang S.P <anangjetbus2@gmail.com>
2026-07-02 05:59:00 +0700

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#FF2D20]'>Anang</span>
+            Hi, I'm <span className='text-[#FF2D20]'>Anang SP</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
